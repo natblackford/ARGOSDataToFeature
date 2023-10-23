2ce65e86d911c6f89ca8749aa9b5a3ea30540c13
natblackford <natsblackford@gmail.com>
2023-10-23 09:17:18 -0400

    beginning of script to read argos data

---
+++ import_argos.py
@@ -0,0 +1,18 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2023
+## Author: nsb32@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
+
+# Set input variables (Hard-wired)
+inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
